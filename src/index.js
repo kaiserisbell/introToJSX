b250f5d025366618bebc919955835bc1372370d1
kaiserisbell <kaiserisbell96@gmail.com>
2021-02-03 12:10:54 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+//const React = require("react");
+//or for es6
+import React from "react";
+
+//const reactDOM = require("react-dom");
+//or for es6
+import reactDOM from "react-dom";
+
+reactDOM.render(
+  <div>
+    <h1>Hello World</h1>
+    <h2>This is react</h2>
+    <ul>
+      <li>item1</li>
+      <li>item2</li>
+      <li>item3</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
